b3af41c8dde1da5a02cf1c3c96efb694bf0053d1
luigibustos <louisalphonsobustos@gmail.com>
2023-03-06 12:34:41 -0800

    Creating Mobile nav button and display

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
 const input = document.getElementById("link-input");
 const linkForm = document.getElementById("link-form");
 const errMsg = document.getElementById("err-msg");
 
+btn.addEventListener("click", navToggle);
 linkForm.addEventListener("submit", formSubmit);
 
-// Validate a URL
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
 function validURL(str) {
   var pattern = new RegExp(
     "^(https?:\\/\\/)?" + // protocol
